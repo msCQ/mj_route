b209504f0cd60c774504f9e9bc9a7f655a849bb7
xuzhuoshuai <xuzhuoshuai@imeijian.cn>
2017-06-25 11:20:20 +0800

    新增posscss配置

--- webpack.config.js
+++ webpack.config.js
@@ -52,11 +52,12 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader", "postcss-loader")
             },
             {
                 test: /\.less$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!less-loader?strictMath&noIeCompat')
+                //  postcss schema-utils
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!less-loader?strictMath&noIeCompat!postcss-loader')
             },
             {
                 test: /\.(png|jpe?g|gif)$/,
